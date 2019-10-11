ce2c03db1378deb7a0f0f048bf3964e1755b9fbc
Rizkifreao <rizkipebrianto96@gmail.com>
2019-10-11 16:49:51 +0700

    update

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -49,34 +49,34 @@
           </div>
         </div>
         <ul class="nav">
-          <li class="nav-item <?= uri_string() == 'dashboard' ? 'active' : '' ?> ">
+          <li class="nav-item <?= $this->uri->segment(1) == 'dashboard' ? 'active' : '' ?> ">
             <a class="nav-link" href="<?=base_url('dashboard') ?>">
               <i class="material-icons">dashboard</i>
               <p> Dashboard </p>
             </a>
           </li>
-          <li class="nav-item <?= uri_string() == 'material' || uri_string() == 'produk' || uri_string() == 'jenis'? 'active' : '' ?>">
+          <li class="nav-item <?= $this->uri->segment(1) == 'material' || $this->uri->segment(1) == 'produk' || $this->uri->segment(1) == 'jenis' || $this->uri->segment(1) == 'satuan' ? 'active' : '' ?>">
             <a class="nav-link" data-toggle="collapse" href="#componentsExamples">
               <i class="material-icons">apps</i>
               <p> Master Data
                 <b class="caret"></b>
               </p>
             </a>
-            <div class="collapse <?=uri_string() == 'material' || uri_string() == 'produk' || uri_string() == 'jenis'? 'show' : '' ?>" id="componentsExamples">
+            <div class="collapse <?=$this->uri->segment(1) == 'material' || $this->uri->segment(1) == 'produk' || $this->uri->segment(1) == 'jenis' || $this->uri->segment(1) == 'satuan' ? 'show' : '' ?>" id="componentsExamples">
               <ul class="nav">
-                <li class="nav-item <?= uri_string() == 'material' ? 'active' : '' ?>">
+                <li class="nav-item <?= $this->uri->segment(1) == 'material' ? 'active' : '' ?>">
                   <a class="nav-link" href="<?=base_url('material') ?>">
                     <i class="material-icons">M</i>
                     <span class="sidebar-normal"> Material </span>
                   </a>
                 </li>
-                <li class="nav-item <?= uri_string() == 'produk' ? 'active' : '' ?>">
+                <li class="nav-item <?= $this->uri->segment(1) == 'produk'? 'active' : '' ?>">
                   <a class="nav-link" href="<?=base_url('produk') ?>">
                     <i class="material-icons">P</i>
                     <span class="sidebar-normal"> Produk </span>
                   </a>
                 </li>
-                 <li class="nav-item <?= uri_string() == 'satuan' ? 'active' : '' ?>">
+                 <li class="nav-item <?= $this->uri->segment(1) == 'satuan'? 'active' : '' ?>">
                   <a class="nav-link" href="<?=base_url('satuan') ?>">
                     <i class="material-icons">S</i>
                     <span class="sidebar-normal"> Satuan </span>
@@ -84,6 +84,12 @@
                 </li>
               </ul>
             </div>
+          </li>
+          <li class="nav-item <?= $this->uri->segment(1) == 'bom'? 'active' : '' ?>">
+            <a class="nav-link" href="<?=base_url('bom') ?>">
+              <i class="material-icons">widgets</i>
+              <p> Bill Of Material </p>
+            </a>
           </li>
           <li class="nav-item <?= $this->uri->segment(1) == 'pembelian'? 'active' : '' ?>">
             <a class="nav-link" data-toggle="collapse" href="#formsExamples">
@@ -103,17 +109,17 @@
               </ul>
             </div>
           </li>
-          <li class="nav-item ">
+          <li class="nav-item <?= $this->uri->segment(1) == 'permintaan'? 'active' : '' ?>">
             <a class="nav-link" data-toggle="collapse" href="#tablesExamples">
               <i class="material-icons">assignment_return</i>
               <p> Barang Keluar
                 <b class="caret"></b>
               </p>
             </a>
-            <div class="collapse" id="tablesExamples">
+            <div class="collapse <?= $this->uri->segment(1) == 'permintaan'? 'show' : '' ?>" id="tablesExamples">
               <ul class="nav">
-                <li class="nav-item ">
-                  <a class="nav-link" href="<?=base_url() ?>/examples/tables/regular.html">
+                <li class="nav-item <?= $this->uri->segment(1) == 'permintaan'? 'active' : '' ?>">
+                  <a class="nav-link" href="<?=base_url('permintaan') ?>">
                     <span class="sidebar-mini"> P </span>
                     <span class="sidebar-normal"> Permintaan Material </span>
                   </a>
